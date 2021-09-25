3b937a08029c35b51d521954e81b51d9541457c6
Norbert Langner <norbitor@gmail.com>
2021-09-25 20:57:11 +0200

    Add Deployment workflow definition

--- tf/variables.tf
+++ tf/variables.tf
@@ -6,11 +6,13 @@ variable "cloudflare_mail" {
 variable "cloudflare_api_key" {
   description = "Cloudflare API Key"
   type        = string
+  sensitive   = true
 }
 
 variable "cloudflare_zone_id" {
   description = "Cloudflare Zone ID"
   type        = string
+  sensitive   = true
 }
 
 variable "aws_profile" {
